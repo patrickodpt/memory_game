b1f80f8e421797b9d0845ad2c6e2023b256e5178
Patrick O'Shea <patrickodpt@gmail.com>
2019-07-10 15:42:31 -0400

    not done yet

--- memory_game-master/wdi-fundamentals-memorygame/memory_game/js/main.js
+++ memory_game-master/wdi-fundamentals-memorygame/memory_game/js/main.js
@@ -2,22 +2,22 @@ var cards = [
   {
     rank: "queen",
     suit: "hearts",
-    cardImage: "images/queen-of-hearts.png"
+    cardImage: "images/images/queen-of-hearts.png"
   },
   {
     rank: "queen",
     suit: "diamonds",
-    cardImage: "images/queen-of-diamonds.png"
+    cardImage: "images/images/queen-of-diamonds.png"
   },
   {
     rank: "king",
     suit: "hearts",
-    cardImage: "images/king-of-hearts.png"
+    cardImage: "images/images/king-of-hearts.png"
   },
   {
     rank: "king",
     suit: "diamonds",
-    cardImage: "images/king-of-diamonds.png"
+    cardImage: "images/images/king-of-diamonds.png"
   }
 ];
 
@@ -25,21 +25,32 @@ var cardsInPlay = [];
 
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
   } else {
-    console.log("Sorry, try again.");
+    alert("Sorry, try again.");
   }
-};
+}
 
-function flipCard(cardId) {
+function flipCard() {
+  var cardId = this.getAttribute('data-id')
   console.log("User flipped " + cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage)
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }
 }
 
-flipCard(0);
-flipCard(2);
+
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src','images/images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    //need to append cardElement to game-board
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+}
+
+createBoard();
